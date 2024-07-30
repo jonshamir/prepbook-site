fefc99a711ff87ec6ac7314da40dec30ec4319bb
Jon Shamir <jon.shamir@gmail.com>
2024-07-30 23:47:17 +0300

    Email form

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,6 +3,20 @@ export function Hero() {
     <header className="Hero">
       <h1>Prepbook</h1>
       <p>Clever recipe manager</p>
+      <br />
+      <form
+        method="post"
+        action="https://app.loops.so/api/newsletter-form/YOUR_FORM_ENDPOINT"
+      >
+        <input
+          type="email"
+          name="email"
+          placeholder="you@example.com"
+          required
+        />
+        <input type="hidden" name="userGroup" value="Website signups" />
+        <button type="submit">Join Waitlist</button>
+      </form>
     </header>
   );
 }
