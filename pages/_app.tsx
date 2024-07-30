ed0b0534f979e277dd7a408a0ed0c943bf88fed0
Jon Shamir <jon.shamir@gmail.com>
2024-07-30 22:56:07 +0300

    Add posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,19 @@ import "@fontsource/lexend";
 import "@fontsource/ia-writer-quattro";
 
 import "../styles/main.css";
+import posthog from "posthog-js";
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
-// if (typeof window !== "undefined") {
-//   posthog.init("phc_Ud6pwqtRXUUeeC5zAjDoPZ7MYE41EdRWPMY2gdni1Yt", {
-//     api_host: "https://app.posthog.com" || "https://us.i.posthog.com",
-//     person_profiles: "identified_only",
-//     // Enable debug mode in development
-//     loaded: (posthog) => {
-//       if (process.env.NODE_ENV === "development") posthog.debug();
-//     },
-//   });
-// }
+if (typeof window !== "undefined") {
+  posthog.init("phc_iJ3jYVyqpVHt2kR5DZB6gUwlPD08QkMvcpnXdtHNWtZ", {
+    api_host: "https://app.posthog.com" || "https://us.i.posthog.com",
+    person_profiles: "identified_only",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
